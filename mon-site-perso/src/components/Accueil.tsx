2b4d7f237d79e7fb5b142450b3e9fde8f451081d
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-25 17:53:12 +0200

    page accueil

---
+++ mon-site-perso/src/components/Accueil.tsx
@@ -0,0 +1,14 @@
+// src/components/Accueil.tsx
+import React from 'react';
+
+const Accueil: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur mon site personnel</h1>
+      <p>Je suis actuellement en 3A en informatique à Polytech Lyon.</p>
+      <p>Mon objectif est de devenir data scientist.</p>
+    </div>
+  );
+};
+
+export default Accueil;
