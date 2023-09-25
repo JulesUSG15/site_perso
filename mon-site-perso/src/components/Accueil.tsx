230fa99f5d753a1b92fedaf8d223fc1ea5f604ca
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-25 18:13:59 +0200

    acceuil styles

--- mon-site-perso/src/components/Accueil.tsx
+++ mon-site-perso/src/components/Accueil.tsx
@@ -1,5 +1,7 @@
 // src/components/Accueil.tsx
 import React from 'react';
+import './Acceuil.scss';
+
 
 const Accueil: React.FC = () => {
   return (
@@ -7,6 +9,29 @@ const Accueil: React.FC = () => {
       <h1>Bienvenue sur mon site personnel</h1>
       <p>Je suis actuellement en 3A en informatique à Polytech Lyon.</p>
       <p>Mon objectif est de devenir data scientist.</p>
+
+      <h2>À propos de moi</h2>
+      <p>
+        Je suis un étudiant passionné par l'ingénierie en informatique et
+        l'analyse de données. Mon parcours académique à Polytech Lyon m'a
+        permis d'acquérir des compétences en programmation, en mathématiques
+        et en sciences de données.
+      </p>
+
+      <h2>Mes intérêts</h2>
+      <ul>
+        <li>Programmation en Python, Java, JavaScript, etc.</li>
+        <li>Apprentissage automatique (Machine Learning) et IA.</li>
+        <li>Visualisation de données et statistiques.</li>
+        <li>Développement web et applications réactives.</li>
+      </ul>
+
+      <h2>Contactez-moi</h2>
+      <p>
+        Si vous souhaitez en savoir plus sur mes projets ou me contacter pour
+        une opportunité professionnelle, n'hésitez pas à m'envoyer un email à
+        l'adresse : votre@email.com
+      </p>
     </div>
   );
 };
