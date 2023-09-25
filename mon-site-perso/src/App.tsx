b0b79fc935145af28212d38aa2233bc08b5889c8
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-25 18:02:55 +0200

    route

--- mon-site-perso/src/App.tsx
+++ mon-site-perso/src/App.tsx
@@ -1,25 +1,21 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Accueil from './components/Accueil';
+import Projets from './components/Projets';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/projets" element={<Projets />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
