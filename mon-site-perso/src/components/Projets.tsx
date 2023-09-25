7854c5408b080cea06add700ce9b9dfe18124791
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-25 17:54:23 +0200

    page projets

---
+++ mon-site-perso/src/components/Projets.tsx
@@ -0,0 +1,13 @@
+// src/components/Projets.tsx
+import React from 'react';
+
+const Projets: React.FC = () => {
+  return (
+    <div>
+      <h2>Mes Projets</h2>
+      {/* Ajoutez ici la liste de vos projets */}
+    </div>
+  );
+};
+
+export default Projets;
