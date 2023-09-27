93831c7c7b59b252854dbd78bb4ab99954e318e4
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-27 22:56:07 +0200

    avancement du projet

--- mon-site-perso/src/components/Accueil.tsx
+++ mon-site-perso/src/components/Accueil.tsx
@@ -1,27 +1,29 @@
 // src/components/Accueil.tsx
 import React from 'react';
 import '../styles/Accueil.scss'; // Importez le fichier SCSS
+import githubLogo from '../img/github-logo.png';
+import linkedinLogo from '../img/linkedin-logo.png';
 
 const Accueil: React.FC = () => {
   return (
     <div className="accueil-container">
       <div className="first-container">
-        <div className="title-content">
+        <div className="title-container">
           <h1 className="name">Ginhac Jules</h1>
           <p className="summary">
             Passionné par les mathématiques et l'informatique, je suis actuellement en 3A informatique à Polytech Lyon.
-            Je souhaite me spécialiser dans le domaine de la data science et de l'intelligence artificielle.
+            Je souhaite me spécialiser dans le domaine de la Data Science et de l'Intelligence Artificielle.
           </p>
         </div>
-        <div className="image-content">
+        <div className="image-container">
           <img
-            src="/votre-photo.jpg"
-            alt="Votre Photo de Profil"
-            className="profile-image"
+            src={require('../img/ma_photo.jpg')}
+            alt="Photo de Jules Ginhac"
+            className="photo"
           />
         </div>
       </div>
-      <div className="buttons">
+      <div className="buttons-container">
         <a href="/a-propos" className="btn">
           À Propos
         </a>
@@ -29,6 +31,19 @@ const Accueil: React.FC = () => {
           Projets
         </a>
       </div>
+      <footer className="footer">
+        <div className="footer-left">
+          <a href="https://github.com/JulesUSG15" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="GitHub" className="social-logo" />
+          </a>
+          <a href="https://www.linkedin.com/in/jules-ginhac-2b8a65236/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinLogo} alt="LinkedIn" className="social-logo" />
+          </a>
+        </div>
+        <div className="footer-right">
+          <p className="footer-text">© 2023 - Ginhac Jules</p>
+        </div>
+      </footer>
     </div>
   );
 };
--- mon-site-perso/src/components/Navigation.tsx
+++ mon-site-perso/src/components/Navigation.tsx
@@ -7,21 +7,25 @@ const Navigation: React.FC = () => {
   return (
     <nav className="navigation">
       <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Accueil
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/a-propos" className="nav-link">
-            À Propos
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/projets" className="nav-link">
-            Projets
-          </Link>
-        </li>
+        <ul className="nav-list1">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Accueil
+            </Link>
+          </li>
+        </ul>
+        <ul className="nav-list2">
+          <li className="nav-item">
+            <Link to="/a-propos" className="nav-link">
+              À Propos
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projets" className="nav-link">
+              Projets
+            </Link>
+          </li>
+        </ul>
       </ul>
     </nav>
   );
